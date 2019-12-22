956e85392c82e537079b90994e0ecf01452acd33
Jaehyuk Lee <jhlumd@gmail.com>
2019-12-21 23:08:17 -0800

    Separate components into different files

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import Square from "./square";
 
-class Square extends React.Component {
-  render() {
-    return <button className="square">{/* TODO */}</button>;
-  }
-}
-
-class Board extends React.Component {
+export default class Board extends React.Component {
   renderSquare(i) {
     return <Square />;
   }
@@ -38,23 +31,3 @@ class Board extends React.Component {
     );
   }
 }
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
-
-ReactDOM.render(<Game />, document.getElementById("root"));
